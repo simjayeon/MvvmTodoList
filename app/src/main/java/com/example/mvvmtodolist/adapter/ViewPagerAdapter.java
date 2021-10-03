277a5e2de96c693afbed1f5bb0f41684a6a72702
simja <simjayeon98@gmail.com>
2021-10-03 23:32:07 +0900

    viewbinding & ViewPagerAdapter

---
+++ app/src/main/java/com/example/mvvmtodolist/adapter/ViewPagerAdapter.java
@@ -0,0 +1,5 @@
+package com.example.mvvmtodolist.adapter;
+
+public class ViewPagerAdapter{
+
+}
--- app/src/main/java/com/example/mvvmtodolist/view/MainActivity.java
+++ app/src/main/java/com/example/mvvmtodolist/view/MainActivity.java
@@ -5,12 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.mvvmtodolist.R;
+import com.example.mvvmtodolist.adapter.ViewPagerAdapter;
+import com.example.mvvmtodolist.databinding.ActivityMainBinding;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mainBinding; //viewBinding
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+
+        mainBinding.tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
     }
 }
